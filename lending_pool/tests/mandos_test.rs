39e41a2b7cec28662006a688b789e67b2b8198f8
Catalin Neagu <neagu.catalin.f@gmail.com>
2021-08-23 13:19:55 +0300

    tests cleanup

--- lending_pool/tests/mandos_test.rs
+++
@@ -1,9 +0,0 @@
-// #[test]
-// fn test_deploy() {
-//     elrond_wasm_debug::mandos_go("mandos/lending_pool-init.scen.json");
-// }
-
-// #[test]
-// fn test_depit() {
-//     elrond_wasm_debug::mandos_go("mandos/lending_pool_depoxit_output.scen.json");
-// }
