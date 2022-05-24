66e80883bb17ed62e5eb71fb872ca5a68eb2a1b9
Costin Carabaș <costin.carabas@gmail.com>
2022-05-24 18:55:36 +0300

    Add helper functions to Interactors
    Bring endpoints from Lending and Liquidity SC to the interactors.

--- erdjs-snippets/lending-pool/liquidityPoolInteractor.ts
+++ erdjs-snippets/lending-pool/liquidityPoolInteractor.ts
@@ -1,6 +1,10 @@
 import path from "path";
 import { IAddress, Interaction, ResultsParser, SmartContract, SmartContractAbi, TransactionWatcher } from "@elrondnetwork/erdjs";
+<<<<<<< HEAD
 import { IAudit, INetworkConfig, INetworkProvider, ITestSession, ITestUser, loadAbiRegistry, loadCode } from "@elrondnetwork/erdjs-snippets";
+=======
+import { INetworkProvider, ITestSession, ITestUser, loadAbiRegistry } from "@elrondnetwork/erdjs-snippets";
+>>>>>>> Add helper functions to Interactors
 import { NetworkConfig } from "@elrondnetwork/erdjs-network-providers";
 
 const PathToLiquidityAbi = path.resolve(__dirname, "..", "..", "liquidity_pool", "output", "liquidity-pool.abi.json");
@@ -47,4 +51,63 @@ export class LiquidityPoolInteractor {
         return firstValue!.valueOf().toString();
     }
 
+    async getPoolAsset(): Promise<string> {
+        // Prepare the interaction, check it, then build the query:
+        let interaction = <Interaction>this.contract.methods.getPoolAsset();
+        let query = interaction.check().buildQuery();
+
+        // Let's run the query and parse the results:
+        await this.networkProvider.queryContract(query);
+        let queryResponse = await this.networkProvider.queryContract(query);
+        let { firstValue } = this.resultsParser.parseQueryResponse(queryResponse, interaction.getEndpoint());
+
+        // Now let's interpret the results.
+        return firstValue!.valueOf().toString();
+    }
+
+    async getPoolParams(): Promise<string> {
+        // Prepare the interaction, check it, then build the query:
+        let interaction = <Interaction>this.contract.methods.getPoolParams();
+        let query = interaction.check().buildQuery();
+
+        // Let's run the query and parse the results:
+        await this.networkProvider.queryContract(query);
+        let queryResponse = await this.networkProvider.queryContract(query);
+        let { firstValue } = this.resultsParser.parseQueryResponse(queryResponse, interaction.getEndpoint());
+
+        // Now let's interpret the results.
+        return firstValue!.valueOf().toString();
+    }
+
+
+    async getAggregatorAddress(): Promise<string> {
+        // Prepare the interaction, check it, then build the query:
+        let interaction = <Interaction>this.contract.methods.getAggregatorAddress();
+        let query = interaction.check().buildQuery();
+
+        // Let's run the query and parse the results:
+        await this.networkProvider.queryContract(query);
+        let queryResponse = await this.networkProvider.queryContract(query);
+        let { firstValue } = this.resultsParser.parseQueryResponse(queryResponse, interaction.getEndpoint());
+
+        console.log(`getAggregatorAddress = ${firstValue!.valueOf().toString()}`)
+        // Now let's interpret the results.
+        return firstValue!.valueOf().toString();
+    }
+
+    async getReserves(): Promise<string> {
+        // Prepare the interaction, check it, then build the query:
+        let interaction = <Interaction>this.contract.methods.getReserves();
+        let query = interaction.check().buildQuery();
+
+        // Let's run the query and parse the results:
+        await this.networkProvider.queryContract(query);
+        let queryResponse = await this.networkProvider.queryContract(query);
+        let { firstValue } = this.resultsParser.parseQueryResponse(queryResponse, interaction.getEndpoint());
+
+        console.log(`Reserves are = ${firstValue!.valueOf().toString()}`)
+        // Now let's interpret the results.
+        return firstValue!.valueOf().toString();
+    }
+    
 }
